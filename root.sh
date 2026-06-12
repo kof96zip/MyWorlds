f38d8a79c8ae1e861d4a68e21554bf9c5f77109b
kof96zip <kof96zip@gmail.com>
2026-06-12 14:44:36 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,76 +1,103 @@
 #!/bin/sh
 
-ROOTFS_DIR=$(pwd)
-export PATH=$PATH:~/.local/usr/bin
+start_proot() {
+    ROOTFS="$1"
+
+    if [ ! -x "$ROOTFS/usr/local/bin/proot" ]; then
+        echo "ERROR: proot not found in $ROOTFS/usr/local/bin/proot"
+        exit 1
+    fi
+
+    exec "$ROOTFS/usr/local/bin/proot" \
+        --rootfs="$ROOTFS" \
+        -0 \
+        -w "/root" \
+        -b /dev \
+        -b /proc \
+        -b /sys \
+        -b /etc/resolv.conf \
+        --kill-on-exit
+}
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+ROOTFS_DIR="$SCRIPT_DIR"
+
+if [ -d "$ROOTFS_DIR/MyWorlds/etc" ] && [ -d "$ROOTFS_DIR/MyWorlds/usr" ]; then
+    ROOTFS_DIR="$ROOTFS_DIR/MyWorlds"
+fi
+
+export PATH="$PATH:$HOME/.local/usr/bin"
 ARCH=$(uname -m)
 
 if [ "$ARCH" = "x86_64" ]; then
-  ARCH_ALT=x86_64
-  ALPINE_VER_URL="https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/x86_64/alpine-minirootfs-3.22.0-x86_64.tar.gz"
+    ARCH_ALT="amd64"
+    UBUNTU_URL="http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.4-base-${ARCH_ALT}.tar.gz"
+    ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/x86_64/alpine-minirootfs-3.22.0-x86_64.tar.gz"
 elif [ "$ARCH" = "aarch64" ]; then
-  ARCH_ALT=aarch64
-  ALPINE_VER_URL="https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/aarch64/alpine-minirootfs-3.22.0-aarch64.tar.gz"
+    ARCH_ALT="arm64"
+    UBUNTU_URL="http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.4-base-${ARCH_ALT}.tar.gz"
+    ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/aarch64/alpine-minirootfs-3.22.0-aarch64.tar.gz"
 else
-  printf "Unsupported CPU architecture: ${ARCH}\n"
-  exit 1
+    echo "Unsupported CPU architecture: $ARCH"
+    exit 1
 fi
 
-if [ ! -e $ROOTFS_DIR/.installed ]; then
-  echo "#######################################################################################"
-  echo "#"
-  echo "#                                  Foxytoux INSTALLER (Alpine 3.22)"
-  echo "#"
-  echo "#                           Copyright (C) 2026, RecodeStudios.Cloud"
-  echo "#"
-  echo "#######################################################################################"
-
-  read -p "Do you want to install Alpine 3.22? (YES/no): " install_alpine
-fi
-
-case $install_alpine in
-  [yY][eE][sS])
-    echo "Downloading Alpine 3.22 rootfs..."
-    curl -L -o /tmp/rootfs.tar.gz "$ALPINE_VER_URL"
-    mkdir -p $ROOTFS_DIR
-    tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
-    ;;
-  *)
-    echo "Skipping Alpine installation."
-    ;;
-esac
-
-if [ ! -e $ROOTFS_DIR/.installed ]; then
-  mkdir -p $ROOTFS_DIR/usr/local/bin
-  curl -L -o $ROOTFS_DIR/usr/local/bin/proot \
-    "https://raw.githubusercontent.com/kof99zip/MyWorlds/main/proot-${ARCH}"
-
-  while [ ! -s "$ROOTFS_DIR/usr/local/bin/proot" ]; do
-    rm -f $ROOTFS_DIR/usr/local/bin/proot
-    curl -L -o $ROOTFS_DIR/usr/local/bin/proot \
-      "https://raw.githubusercontent.com/kof99zip/MyWorlds/main/proot-${ARCH}"
-    sleep 1
-  done
-
-  chmod 755 $ROOTFS_DIR/usr/local/bin/proot
-fi
-
-if [ ! -e $ROOTFS_DIR/.installed ]; then
-  mkdir -p $ROOTFS_DIR/etc
-  printf "nameserver 1.1.1.1\nnameserver 1.0.0.1\n" > $ROOTFS_DIR/etc/resolv.conf
-  touch $ROOTFS_DIR/.installed
-fi
-
-CYAN='\e[0;36m'
-WHITE='\e[0;37m'
-RESET_COLOR='\e[0m'
-
-display_gg() {
-  echo -e ""
+install_ubuntu() {
+    echo "Downloading Ubuntu 20.04 rootfs..."
+    curl -L -o /tmp/rootfs.tar.gz "$UBUNTU_URL"
+    mkdir -p "$ROOTFS_DIR"
+    tar -xf /tmp/rootfs.tar.gz -C "$ROOTFS_DIR"
 }
 
-clear
-display_gg
+install_alpine() {
+    echo "Downloading Alpine 3.22 rootfs..."
+    curl -L -o /tmp/rootfs.tar.gz "$ALPINE_URL"
+    mkdir -p "$ROOTFS_DIR"
+    tar -xf /tmp/rootfs.tar.gz -C "$ROOTFS_DIR"
+}
 
-$ROOTFS_DIR/usr/local/bin/proot \
-  --rootfs="${ROOTFS_DIR}" \
-  -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit
+install_proot() {
+    mkdir -p "$ROOTFS_DIR/usr/local/bin"
+    curl -L -o "$ROOTFS_DIR/usr/local/bin/proot" \
+        "https://raw.githubusercontent.com/kof96zip/MyWorlds/main/proot-${ARCH}"
+
+    while [ ! -s "$ROOTFS_DIR/usr/local/bin/proot" ]; do
+        rm -f "$ROOTFS_DIR/usr/local/bin/proot"
+        curl -L -o "$ROOTFS_DIR/usr/local/bin/proot" \
+            "https://raw.githubusercontent.com/kof96zip/MyWorlds/main/proot-${ARCH}"
+        sleep 1
+    done
+
+    chmod 755 "$ROOTFS_DIR/usr/local/bin/proot"
+}
+
+configure_dns() {
+    mkdir -p "$ROOTFS_DIR/etc"
+    printf "nameserver 1.1.1.1\nnameserver 1.0.0.1\n" > "$ROOTFS_DIR/etc/resolv.conf"
+}
+
+if [ ! -e "$ROOTFS_DIR/.installed" ]; then
+    echo "Select which environment to install:"
+    echo "[1] Ubuntu 20.04"
+    echo "[2] Alpine 3.22"
+    read -p "Enter choice [1/2]: " choice
+
+    case "$choice" in
+        1)
+            install_ubuntu
+            ;;
+        2)
+            install_alpine
+            ;;
+        *)
+            echo "Invalid choice, exiting."
+            exit 1
+            ;;
+    esac
+
+    install_proot
+    configure_dns
+    touch "$ROOTFS_DIR/.installed"
+fi
+
+start_proot "$ROOTFS_DIR"
